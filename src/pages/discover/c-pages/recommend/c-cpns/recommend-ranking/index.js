20d5d6a8bfab621add61b2b3cc81dcfe83fc84d6
coderdxh <798616582@qq.com>
2021-04-02 20:47:14 +0800

    recommend over

--- src/pages/discover/c-pages/recommend/c-cpns/recommend-ranking/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/recommend-ranking/index.js
@@ -1,14 +1,20 @@
 import React, { memo, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 
 import { getTopListAction } from '../../store/actionCreators';
 
 import XHThemeHeaderRMC from "@/components/theme-header-rcm";
+import XHTopRanking from "@/components/top-ranking";
 import { RankingWrapper } from "./style";
 
 export default memo(function XHRecommendRanking() {
 
   const dispatch = useDispatch();
+  const { upRanking, newRanking, originRanking } = useSelector(state => ({
+    upRanking: state.getIn(["recommend", "upRanking"]),
+    newRanking: state.getIn(["recommend", "newRanking"]),
+    originRanking: state.getIn(["recommend", "originRanking"])
+  }), shallowEqual);
 
   useEffect(() => {
     dispatch(getTopListAction(0))
@@ -19,6 +25,11 @@ export default memo(function XHRecommendRanking() {
   return (
     <RankingWrapper>
       <XHThemeHeaderRMC title="榜单" />
+      <div className="tops">
+        <XHTopRanking info={upRanking} />
+        <XHTopRanking info={newRanking} />
+        <XHTopRanking info={originRanking} />
+      </div>
     </RankingWrapper>
   )
 })
--- src/pages/discover/c-pages/recommend/c-cpns/recommend-ranking/style.js
+++ src/pages/discover/c-pages/recommend/c-cpns/recommend-ranking/style.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
+import recommend_top_bg from "@/assets/img/recommend-top-bg.png"
+
 export const RankingWrapper = styled.div`
   .tops {
     margin: 30px 0;
     display: flex;
-    background-image: url(${require("@/assets/img/recommend-top-bg.png")});
+    background-image: url(${recommend_top_bg});
     height: 472px;
   }
 `
